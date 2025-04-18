4c2341cd6f40c1d017ee9ade302537c6b6eedd59
EC2 Default User <ec2-user@ip-172-31-2-159.ec2.internal>
2025-04-18 08:34:05 +0000

    update deploy-lambda 2

--- cloudformation/deploy-lambda.sh
+++ cloudformation/deploy-lambda.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
 # 변수 설정
-GITHUB_REPO="https://github.com/kjhyuok/genai-chatops"
-LOCAL_DIR="genai-chatops"
 LAMBDA_FUNCTION_NAME_1="chatops-gw-to-slack-function"
 LAMBDA_FUNCTION_NAME_2="chatops-msg-to-slack-function"
 REGION="us-west-2"  # AWS 리전
-
-# 1. GitHub에서 소스 클론
-echo "Cloning repository from GitHub..."
-git clone $GITHUB_REPO $LOCAL_DIR
-cd $LOCAL_DIR
-cd cloudformation
 
 # 2. Lambda 함수 코드 업데이트
 echo "Updating Lambda function code..."
@@ -24,6 +16,5 @@ aws lambda update-function-code \
     --function-name $LAMBDA_FUNCTION_NAME_2 \
     --zip-file fileb://lambda-message-to-slack.zip \
     --region $REGION
-
 
 echo "Deployment completed successfully!"
